8d16d7465d38fa602f7230dcda8ece1c65c69e99
Zahra Roshani <zahraroshani973@gmail.com>
2024-06-05 11:49:37 +0330

    signup page added

---
+++ lib/signup.dart
@@ -0,0 +1,126 @@
+import 'package:appppp/login.dart';
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.indigo,),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 80),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                    child: Image.asset(
+                      'assets/university_logo.jpg',
+                      height: 200,
+                    ),
+                  ),
+                SizedBox(height: 20),
+                Center(
+                  child: Text(
+                    'Join Our University',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Create an Account to Get Started',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 40),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    filled: true,
+                    fillColor: Colors.grey[200], // Background color
+                    border: OutlineInputBorder( // Border
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    prefixIcon: Icon(Icons.person, color: Colors.grey), // Icon
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'CodeId',
+                    filled: true,
+                    fillColor: Colors.grey[200], // Background color
+                    border: OutlineInputBorder( // Border
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    prefixIcon: Icon(Icons.email, color: Colors.grey), // Icon
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    filled: true,
+                    fillColor: Colors.grey[200], // Background color
+                    border: OutlineInputBorder( // Border
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    prefixIcon: Icon(Icons.lock, color: Colors.grey), // Icon
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(style: ButtonStyle(backgroundColor:MaterialStateProperty.all(Colors.indigo)),
+
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 18,color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Already have an account? Login', style: TextStyle(color: Colors.black)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
