88cb5604a5ae29386350ea091be06e0982ee9084
Zahra Roshani <zahraroshani973@gmail.com>
2024-07-06 00:10:14 +0330

    update Server file for different login cases

--- Server/Server.java
+++ Server/Server.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import Model.StudentModel;
+
 public class Server {
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to the server!");
@@ -31,7 +33,7 @@ class ClientHandler extends Thread {
     }
 
     public String listener() throws IOException {
-        System.out.println("listener");
+        System.out.println("listener is activated");
         StringBuilder sb = new StringBuilder();
         int index = dis.read();
         while (index != 0) {
@@ -39,7 +41,7 @@ class ClientHandler extends Thread {
             index = dis.read();
 
         }
-        System.out.println("Listener");
+        System.out.println("Listener2 -> read command successfully");
         return sb.toString();
     }
 
@@ -72,7 +74,7 @@ class ClientHandler extends Thread {
                 boolean signedIn = false;
                 int responseofDatabase = 100;
                 try {
-                    responseofDatabase = DataBase.usersChecker(split[1], split[2]);
+                    responseofDatabase = StudentModel.Login(split[1], split[2]);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -82,6 +84,24 @@ class ClientHandler extends Thread {
                     System.out.println("Successfuly logged in!");
                     try {
                         writer("200");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else if (responseofDatabase == 1) {
+                    signedIn = false;
+                    System.out.println("status code is 401");
+                    System.out.println("password is incorrect");
+                    try {
+                        writer("401");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else if (responseofDatabase == 0) {
+                    signedIn = false;
+                    System.out.println("status code is 404");
+                    System.out.println("User not found");
+                    try {
+                        writer("404");
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
