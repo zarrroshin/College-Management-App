a6861a06cd407b502aa21337d06ab9c080cd335f
Zahra Roshani <zahraroshani973@gmail.com>
2024-07-06 16:52:49 +0330

    change pubspec for toasting notification

---
+++ Backend/src/Server/Server.java
@@ -0,0 +1,117 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to the server!");
+        ServerSocket serverSocket = new ServerSocket(8080);
+        while (true) {
+            System.out.println("Waiting for client");
+            new ClientHandler(serverSocket.accept()).start();
+
+        }
+
+    }
+}
+
+class ClientHandler extends Thread {
+    Socket socket;
+    DataOutputStream dos;
+    DataInputStream dis;
+    StudentModel studentModel = new StudentModel();
+
+    ClientHandler(Socket socket) throws IOException {
+        this.socket = socket;
+        dos = new DataOutputStream(socket.getOutputStream());
+        dis = new DataInputStream(socket.getInputStream());
+        System.out.println("connected to server");
+
+    }
+
+    public String listener() throws IOException {
+        System.out.println("listener is activated");
+        StringBuilder sb = new StringBuilder();
+        int index = dis.read();
+        while (index != 0) {
+            sb.append((char) index);
+            index = dis.read();
+        }
+        System.out.println("Listener2 -> read command successfully");
+        return sb.toString();
+    }
+
+    public void writer(String write) throws IOException {
+        dos.writeBytes(write);
+        dos.flush();
+        dos.close();
+        dis.close();
+        socket.close();
+        System.out.println(write);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        String command;
+        try {
+            command = listener();
+            System.out.println("command recived {" + command + "}");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] split = command.split("~");
+        for (String s : split) {
+            System.out.println(s);
+        }
+        switch (split[0]) {
+            case "GET: loginChecker": {
+                boolean signedIn = false;
+                int responseofDatabase = 100;
+                try {
+
+                    responseofDatabase = studentModel.Login(split[1], split[2]);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (responseofDatabase == 2) {
+                    signedIn = true;
+                    System.out.println("status code is 200");
+                    System.out.println("Successfuly logged in!");
+                    try {
+                        writer("200");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else if (responseofDatabase == 1) {
+                    signedIn = false;
+                    System.out.println("status code is 401");
+                    System.out.println("password is incorrect");
+                    try {
+                        writer("401");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else if (responseofDatabase == 0) {
+                    signedIn = false;
+                    System.out.println("status code is 404");
+                    System.out.println("User not found");
+                    try {
+                        writer("404");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+                break;
+
+            default:
+                break;
+        }
+
+    }
+}
---
+++ Backend/src/Server/StudentModel.java
@@ -0,0 +1,83 @@
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StudentModel {
+    private static final String FILE_PATH = "C:\\Users\\Lenovo\\OneDrive\\deks\\AP_PROJECT\\College-Management-App\\Backend\\data\\student.txt";
+    private static List<String[]> students;
+
+    public StudentModel() {
+        students = new ArrayList<String[]>();
+        loadStudents();
+    }
+
+    public void loadStudents() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.trim().indexOf('*') == 0)
+                    continue;
+                String[] student_detail = line.split(",");
+                if (student_detail.length >= 3)
+                    this.students.add(student_detail);
+            }
+        } catch (IOException e) {
+            System.out.println("Error!!");
+            e.printStackTrace();
+        }
+    }
+
+    public Boolean CheckUnique(List<String> register_list) {
+        // register list --- > username(0) codeID(1) ---> () means index length of list
+        // is 2
+        String username = register_list.get(0);
+        String codeID = register_list.get(1);
+
+        for (String[] student : students) {
+            if (student[0].equalsIgnoreCase(username) || student[1].equalsIgnoreCase(codeID)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static int Login(String username_or_codeID, String password) {
+        int result = 0;
+        for (String[] student : students) {
+            if ((student[0].equalsIgnoreCase(username_or_codeID)
+                    || student[1].equalsIgnoreCase(username_or_codeID))) {
+                result = 1;
+                if (student[2].equalsIgnoreCase(password)) {
+                    result = 2;
+                    return result;
+                }
+
+            }
+
+        }
+        return result;
+
+    }
+
+    public Boolean AddStudentToDataBase(List<String> register_list) {
+        String text = register_list.get(0) + ","
+                + register_list.get(1) + ","
+                + register_list.get(2);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true));
+            writer.newLine();
+            writer.write(text);
+            writer.close();
+            students.add(register_list.toArray(new String[0]));
+            return true;
+        } catch (IOException ioe) {
+            System.out.println("Something Went Wrong Please try again!!!");
+            ioe.printStackTrace();
+            return false;
+        }
+    }
+
+}
